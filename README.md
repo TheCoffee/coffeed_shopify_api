7c4595516bd7e4afceab1e676b62a891be6c8b37
Ghost <gauravgpunjabi@gmail.com>
2023-07-11 16:36:28 +0530

    Shopify API connection setup

---
+++ README.md
@@ -0,0 +1,22 @@
+# Getting Started with the Shopify Storefront API and Node.js
+
+Shopify provides a versatile Storefront API that allows us  to access a full range of commerce options. The API makes it possible for customers to view products and collections, add products to a cart, and check out.
+
+This repo is an extension of the Medium article on Coffeed that deals with integrating the Storefront API to a Node.js web app. The project uses express to create the routes to access the API.
+
+## Installing dependencies
+
+Install project dependencies by running:
+```bash
+yarn
+```
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+node index.js
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result. You can start editing the API by modifying `index.js`.
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 require('dotenv').config();
 
-const app = express();
-const port = 3001;
+require("@shopify/shopify-api/adapters/node");
+const { shopifyApi, LATEST_API_VERSION } = require("@shopify/shopify-api");
 
-app.get("/", (req, res) => {
-    res.send("Hello Coffeed!");
-})
+const shopify = shopifyApi({
+    apiKey: process.env.CLIENT_ID,
+    apiSecretKey: process.env.CLIENT_KEY,
+    scopes: ["read_products"],
+    hostName: process.env.HOST_NAME,
+});
+
+const app = express();
+const port = 3000;
+
+const storefrontAccessToken = process.env.STOREFRONT_ACCESS_TOKEN;
+const shop = process.env.STORE_URL;
+
+const storefrontClient = new shopify.clients.Storefront({
+    domain: shop,
+    storefrontAccessToken,
+});
+
+app.get("/", async (req, res) => {
+    const products = await storefrontClient.query({
+        data: `{
+            products (first: 3) {
+            edges {
+                node {
+                    id
+                    title
+                }
+            }
+            }
+        }`,
+    });
+
+    res.send(products);
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
